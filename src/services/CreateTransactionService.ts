01677c49644b8c9337e7012b25c3d92f6c0d480b
Everton Araujo <everton.aaraujo@outlook.com>
2020-09-13 14:30:42 -0300

    :sparkles: challenge finalized

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,11 +1,55 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
-  }
+  public async execute({ title, value, type, category }: RequestDTO): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    const categoryRepository = getRepository(Category);
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('Outcome value should be higher than total amount');
+    }
+
+    let transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(transactionCategory);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
+  };
 }
 
 export default CreateTransactionService;
